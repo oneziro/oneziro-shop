9693fc512edb62e7551d537249f0d10522187317
ashkanfekri <ashkanfekridev@gmail.com>
2024-09-29 22:09:18 +0330

    Create 2024_09_29_182238_create_categories_table.php
    create migration for categories

---
+++ database/migrations/2024_09_29_182238_create_categories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Category;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('url');
+            $table->string('description')->nullable();
+            $table->foreignIdFor(Category::class)->nullable();
+            $table->timestamp('published_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
